9443e954a6a69098caccff392c9c42f184b1d930
Anthony Mays <`````>
2021-09-18 16:32:24 -0500

    readme check in

--- README.md
+++ README.md
@@ -9,8 +9,12 @@ This class is not backwards compatible to PHP 5 because it uses scalar and retur
 type declarations
 ```
 
+## Package Requirements
+* Each REST call is a separate call
+* Results from previous calls are your responsibility to manage to and including headers, bodies and options
+* This package will throw only those errors that prevent a REST call from being made or response received
 
-## CURL OPTS
+## cURL Options
 The cURL options rules apply to this package
 * Individual cURL options are not validated with this package
 * cURL options can be managed by the following methods:
@@ -20,6 +24,7 @@ clear_options - clears all previous options
 delete_options - deletes a specified option
 get_options - gets the current options specified as an array
 ```
+* The add_option(...) method will ignore a new option if the option has already been established
 * Object cURL options can be set with the class constructor to establish a starting point
 * Each HTTP Method (GET, POST, etc) will set the basic cURL options for class call convenience
 * Review the link below for available cURL options available for each REST call
--- rest_client.php
+++ rest_client.php
@@ -2,102 +2,16 @@
 declare(strict_types = 1);
 
 class RESTClient {
-	private $request = [
-		'url'=>'',
-		'method'=>'',
-		'headers'=>[],
-		'options'=>[],
-		'body'=>'',
-	];
+	private $curl_opts;
 
-	private $response = [
-		'headers'=>[],
-		'body'=>'',
-		'http_code'=>404,
-	];
-
-	public function __construct() {}
-	public function __destruct() {}
-
-	// actions
-
-	public function exec() {
-
+	public function __construct(array $curl_opts = []) {
+		$this->curl_opts = [];
 	}
 
-	public function post() {
-
+	public function __destruct() {
+		$this->curl_opts = null;
 	}
 
-	public function patch() {
-
-	}
-
-	public function put() {
-
-	}
-
-	public function get() {
-
-	}
-
-	public function head() {
-
-	}
-
-	public function options() {
-
-	}
-
-	// getters
-
-	final public function get_request() : array {
-		return $this->request;
-	}
-
-	final public function get_request_url() : string {
-		return $this->request['url'];
-	}
-
-	final public function get_request_method() : string {
-		return $this->request['method'];
-	}
-
-	final public function get_request_headers() : array {
-		return $this->request['headers'];
-	}
-
-	final public function get_request_options() : array {
-		return $this->request['options'];
-	}
-
-	final public function get_request_body() : string {
-		return $this->request['body'];
-	}
-
-	final public function get_response() : array {
-		return $this->response;
-	}
-
-	final public function get_response_headers() : array {
-		return $this->response['headers'];
-	}
-
-	final public function get_response_body() : string {
-		return $this->response['body'];
-	}
-
-	final public function get_response_http_code() : int {
-		return $this->response['http_code'];
-	}
-
-
-
-
-	// clear
-	public function clear_request() : void {
-
-	}
 
 
 
